bfe9e6aa578ca934b49c55c95e9c40044eba22d2
Dup4 <lyuzhi.pan@gmail.com>
2022-05-08 02:25:34 +0000

    chore: fix lint args

--- src/nativeAddon.ts
+++ src/nativeAddon.ts
@@ -1,4 +1,4 @@
-import * as bindings from "bindings";
+import bindings from "bindings";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const sandboxAddon: any = bindings("sandbox");
 
