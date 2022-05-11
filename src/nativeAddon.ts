b5a823f9866974e1e7ccaa00d9dd0cfe3004aac9
Dup4 <lyuzhi.pan@gmail.com>
2022-05-11 15:39:04 +0000

    fix: binding name

--- src/nativeAddon.ts
+++ src/nativeAddon.ts
@@ -1,5 +1,7 @@
 import bindings from "bindings";
+
+// The name of this binding needs to correspond to the project name in `CMakeLists.txt`
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const sandboxAddon: any = bindings("sandbox");
+const sandboxAddon: any = bindings("simple-sandbox");
 
 export default sandboxAddon;
